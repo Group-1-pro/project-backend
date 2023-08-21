183750d804416c7e90b4e334d0deb92451c629b3
shnorlaxzz <mshnas56@gmail.com>
2023-08-21 20:28:35 +0000

    ready to deploy

--- project/urls.py
+++ project/urls.py
@@ -46,5 +46,5 @@ urlpatterns = [
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
